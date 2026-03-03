2cb7eb5b8a6809a26f96c7de96a0861d8cd5ea99
Scott Thompson <scott815@gmail.com>
2026-03-02 22:33:14 -0700

    more tag features.

--- scenarios.js
+++ scenarios.js
@@ -6,12 +6,12 @@
  * into the scenario defaults defined in backstop.config.js.
  */
 module.exports = [
-  {"label":"Best Attorneys","path":"/best-attorneys"},
-  {"label":"Best Attorneys Canada","path":"/best-attorneys/canada"},
-  {"label":"Best Attorneys Personal Injury","path":"/best-attorneys/personal-injury"},
-  {"label":"Best Attorneys Canada Personal Injury","path":"/best-attorneys/canada/personal-injury"},
-  {"label":"Best attorney USA State - arizona","path":"/best-attorneys/arizona"},
-  {"label":"Canada by Providence of Ontario","path":"/best-attorneys/canada/ontario"},
-  {"label":"Phoenix Attorneys","path":"/best-attorneys/arizona/phoenix"},
-  {"label":"Attorneys in Toronto, Ontario","path":"/best-attorneys/canada/ontario/toronto"},
+  {"label":"Best Attorneys","path":"/best-attorneys","tags":["usa"]},
+  {"label":"Best Attorneys Canada","path":"/best-attorneys/canada","tags":["canada"]},
+  {"label":"Best Attorneys Personal Injury","path":"/best-attorneys/personal-injury","tags":["usa"]},
+  {"label":"Best Attorneys Canada Personal Injury","path":"/best-attorneys/canada/personal-injury","tags":["canada"]},
+  {"label":"Best attorney USA State - arizona","path":"/best-attorneys/arizona","tags":["usa"]},
+  {"label":"Canada by Providence of Ontario","path":"/best-attorneys/canada/ontario","tags":["canada"]},
+  {"label":"Phoenix Attorneys","path":"/best-attorneys/arizona/phoenix","tags":["usa"]},
+  {"label":"Attorneys in Toronto, Ontario","path":"/best-attorneys/canada/ontario/toronto","tags":["canada"]},
 ];
